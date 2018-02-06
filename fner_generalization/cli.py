543d0331c16e3622a1062756e9243ce66cbe92d9
Sambhav Kothari <sambhavs.email@gmail.com>
2018-02-07 02:06:51 +0530

    Add fine-grained under-sampling

--- fner_generalization/cli.py
+++ fner_generalization/cli.py
@@ -2,14 +2,15 @@ import click
 
 from fner_generalization.indexing import generate_label_mentions, generate_reverse_index
 from fner_generalization.oversampling import over_sample_below_thresh, generate_over_sampled_data
-from fner_generalization.undersampling import under_sample_above_thresh
+from fner_generalization.undersampling import under_sample_above_thresh, under_sample_entity_above_thresh
 
 commands = {
     'generate-mentions': generate_label_mentions,
     'generate-index': generate_reverse_index,
     'oversample': over_sample_below_thresh,
     'generate-oversampled-data': generate_over_sampled_data,
-    'undersample': under_sample_above_thresh
+    'undersample': under_sample_above_thresh,
+    'undersample-entity': under_sample_entity_above_thresh,
 }
 
 fnerg = click.Group(commands=commands)
--- fner_generalization/undersampling.py
+++ fner_generalization/undersampling.py
@@ -6,7 +6,38 @@ import click
 
 from fner_generalization.constants import (SAMPLE_DATA_FILE,
                                          REVERSE_COUNT_INDEX,
-                                         UNDERSAMPLE_DATA)
+                                         UNDERSAMPLE_DATA,
+                                         REVERSE_LABEL_COUNT_INDEX)
+
+
+def under_sample_label_entities(entities, data_file):
+    result = []
+    with open(data_file) as f:
+        for line in f:
+            obj = json.loads(line)
+            new_mentions = []
+            for mention in obj['mentions']:
+                name = mention['name']
+                if name in entities:
+                    name_obj = entities[name]
+                    labels = set(filter(lambda x: name_obj[x] > 0, name_obj))
+                    if not labels:
+                        entities.pop(name)
+                        continue
+                    new_labels = []
+                    for label in mention['labels']:
+                        if label in labels:
+                            name_obj[label] -= 1
+                        else:
+                            new_labels.append(label)
+                    mention['labels'] = new_labels
+                if mention['labels']:
+                    new_mentions.append(mention)
+            if new_mentions:
+                obj['mentions'] = new_mentions
+                result.append(json.dumps(obj) + '\n')
+    with open(UNDERSAMPLE_DATA, 'w') as f:
+        f.writelines(result)
 
 
 def under_sample_entities(entities, n, data_file):
@@ -27,7 +58,7 @@ def under_sample_entities(entities, n, data_file):
                 for name in valid:
                     if count[name] < n:
                         count[name] += 1
-                result.append(json.dumps(line) + '\n')
+                    result.append(json.dumps(line) + '\n')
 
     with open(UNDERSAMPLE_DATA, 'w') as f:
         f.writelines(result)
@@ -45,3 +76,19 @@ def under_sample_above_thresh(threshhold, n, data_file, index_file):
         if int(i) >= threshhold:
             entities += data[i]
     under_sample_entities(entities, n, data_file)
+
+
+@click.command('undersample-entity')
+@click.option('--threshhold', default=10, help="Undersample entities with above `threshhold` number of mentions")
+@click.option('--p', default=80, help="Percent of lines to undersample")
+@click.option('--data_file', type=click.Path(exists=True), default=SAMPLE_DATA_FILE)
+@click.option('--index_file', type=click.Path(exists=True), default=REVERSE_LABEL_COUNT_INDEX)
+def under_sample_entity_above_thresh(threshhold, p, data_file, index_file):
+    data = json.load(open(index_file))
+    entities = defaultdict(lambda: defaultdict(int))
+    for i in data:
+        if int(i) >= threshhold:
+            for entity, value in data[i].items():
+                for label in value:
+                    entities[entity][label] = int(((100.0 - p) / 100.0) * int(i))
+    under_sample_label_entities(entities, data_file)
