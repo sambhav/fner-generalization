5322d04d1d467a97cfb06ea822425f58bed52a1f
Sambhav Kothari <sambhavs.email@gmail.com>
2018-03-20 13:18:43 +0530

    Add name wise under-sampling

--- fner_generalization/cli.py
+++ fner_generalization/cli.py
@@ -2,7 +2,7 @@ import click
 
 from fner_generalization.indexing import generate_label_mentions, generate_reverse_index
 from fner_generalization.oversampling import over_sample_below_thresh, generate_over_sampled_data
-from fner_generalization.undersampling import under_sample_above_thresh, under_sample_entity_above_thresh
+from fner_generalization.undersampling import under_sample_above_thresh, under_sample_entity_above_thresh, under_sample_name_above_thresh
 
 commands = {
     'generate-mentions': generate_label_mentions,
@@ -11,6 +11,7 @@ commands = {
     'generate-oversampled-data': generate_over_sampled_data,
     'undersample': under_sample_above_thresh,
     'undersample-entity': under_sample_entity_above_thresh,
+    'undersample-name': under_sample_name_above_thresh,
 }
 
 fnerg = click.Group(commands=commands)
--- fner_generalization/undersampling.py
+++ fner_generalization/undersampling.py
@@ -58,8 +58,35 @@ def under_sample_entities(entities, n, data_file):
                 for name in valid:
                     if count[name] < n:
                         count[name] += 1
-                    result.append(json.dumps(line) + '\n')
+                result.append(json.dumps(line) + '\n')
+    with open(UNDERSAMPLE_DATA, 'a+') as f:
+        f.writelines(result)
 
+def under_sample_names(entities, data_file, flag=False):
+    result = []
+    en = set(entities)
+    count = defaultdict(int)
+    count.update(entities)
+    with open(data_file) as f:
+        for line in f:
+            line = json.loads(line)
+            mentions = line["mentions"]
+            names = set([mention["name"] for mention in mentions])
+            valid = names & en
+            under_sample = bool(valid)
+            if under_sample:
+                if flag:
+                    under_sample = any(map(lambda x: bool(count[x]), valid))
+                else:
+                    under_sample = all(map(lambda x: bool(count[x]), valid))
+
+            if under_sample:
+                print(valid)
+                for name in valid:
+                    if count[name] > 0:
+                        count[name] -= 1
+            else:
+                result.append(json.dumps(line) + '\n')
     with open(UNDERSAMPLE_DATA, 'w') as f:
         f.writelines(result)
 
@@ -92,3 +119,17 @@ def under_sample_entity_above_thresh(threshhold, p, data_file, index_file):
                 for label in value:
                     entities[entity][label] = int(((100.0 - p) / 100.0) * int(i))
     under_sample_label_entities(entities, data_file)
+
+@click.command('undersample-name')
+@click.option('--threshhold', default=10, help="Undersample entities with above `threshhold` number of mentions")
+@click.option('--p', default=80, help="Percent of lines to undersample")
+@click.option('--data_file', type=click.Path(exists=True), default=SAMPLE_DATA_FILE)
+@click.option('--index_file', type=click.Path(exists=True), default=REVERSE_COUNT_INDEX)
+def under_sample_name_above_thresh(threshhold, p, data_file, index_file):
+    data = json.load(open(index_file))
+    entities = {}
+    for i in data:
+        if int(i) >= threshhold:
+            for entity in data[i]:
+                entities[entity] = int(((100.0 - p) / 100.0) * int(i))
+    under_sample_names(entities, data_file)
