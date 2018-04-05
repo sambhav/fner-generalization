30935cda44f90809f220e74e0f86a43e48e9484f
Sambhav Kothari <sambhavs.email@gmail.com>
2018-04-05 16:19:33 +0530

    Update setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='fnerg',
     version='0.1',
-    include_package_data=True,
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
     ],
